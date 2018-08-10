a5c7a810e2c4df4cfd931d83a6d99876f5d1541c
Yury <y3401@yandex.ru>
2018-08-10 17:26:24 +0300

    Add files via upload

--- modbbcode.py
+++ modbbcode.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#
 
-import re
+import re,webbrowser
 
 smile_start = '<img src="http://static.t-ru.org/smiles/'
 smile_end = '.gif" alt="" />'
@@ -22,7 +21,7 @@ sm={':D':'icon_biggrin',':)':'icon_smile',':(':'icon_sad',':o':'icon_surprised',
 ':ninja:':'ppl_ninja',':ninjajig:':'ppl_ninjajig',':shuriken:':'ppl_shuriken',
 ':oldtimer:':'ppl_oldtimer',':snegurochka:':'snegurochka',':pimp:':'ppl_pimp',
 ':pirat:':'ppl_pirat',':pirate:':'ppl_pirate',':police:':'ppl_police',
-':pop:':'ppl_pop',':priest:':'ppl_priest',':punk:':'ppl_punk',
+':pop:':'ppl_pop',':prist:': 'ppl_priest',':punk:':'ppl_punk',
 ':rambo:':'ppl_rambo',':rock:':'ppl_rock',':newyear:':'new_year',
 ':smurf:':'ppl_smurf',':spidey:':'ppl_spidey',':wolverine:':'ppl_wolverine',
 ':zorro:':'ppl_zorro',':artist:':'ext_artist',':argue:':'ext_argue',
@@ -60,18 +59,123 @@ sm={':D':'icon_biggrin',':)':'icon_smile',':(':'icon_sad',':o':'icon_surprised',
 ':bayan:':'bayan',':kk:':'kak_kino',':-|':'icon_neutral',':search:':'use_search'}
 
 zam = {'[b]':'<span class="post-b">', '[/b]':'</span>',
-    '[i]':'<span class="post-i">', '[/i]':'</span>',
-    '[u]':'<span class="post-u">', '[/u]':'</span>',
+    '[i]':'<i>', '[/i]':'</i>',
+    '[u]':'<u>', '[/u]':'</u>',
     '[s]':'<span class="post-s">', '[/s]':'</span>',
-    '[list]':'<ol class="post-ul">','[list=1]':'<ol class="post-ul">', '[/list]':'</ol>',
+    '[list]':'<ol class="post-ul">', '[/list]':'</ol>',
     '[*]':'</span></li><li><span class="post-font-serif1">',
     '[hr]':'<hr>',
     '[br]':'<span class="post-br"></span>'}
+
+html_header = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru" dir="ltr">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="style.css" />
+<head><title>Инфо о раздаче</title></head><body>
+<script language="JavaScript" type="text/javascript">
+     function showBlock(t) {
+           var elStyle = document.getElementById( t ).style;
+           elStyle.display = (elStyle.display == 'none') ? '' : 'none';}
+</script>
+<div class="post_body">
+"""
+html_end='</div><br /><br /></body></html>'
 
 tags=[('[b]','[/b]'),('[i]','[/i]'),('[u]','[/u]'),('[s]','[/s]'),('[list]','[/list]'),
       ('[size','[/size]'),('[img','[/img]'),('[color','[/color]'),('[font','[/font]'),
       ('[pre','[/pre]'),('[code','[/code]'),('[url','[/utl]'),('[spoiler','[/spoiler]'),
       ('[quote','[/quote]'),('[align','[/align]')]
+
+
+content="""
+[size=24][align=center][b]US Open 2016. 3-й круг
+Daniel EVANS GBR - Stan WAWRINKA SUI [3][/b][/align][/size]
+
+[img=right]http://i82.fastpic.ru/big/2016/0830/b8/198d61f8212dd812c7482b84e53ae1b8.png[/img]
+
+[b]Дата эфира[/b]: 03.09.2016
+[b]Вид спорта[/b]: Теннис
+[b]Комментарий[/b]: Профессиональный
+[b]Язык комментариев[/b]: Русский / Английский
+[hr]
+[b]Описание[/b]: US Open 2016. Матч третьего круга женского одиночного разряда Даниэль Эванс (Великобритания) - Стэн Вавринка (Швейцария).
+
+[b][size=16]Согласно Re Do, на записи есть ошибки. Но не много.[/size][/b]
+[hr]
+[b]Автор рипа[/b]: [i]sanchez2011[/i]
+
+[b]Качество[/b]: SATRemux
+[b]Формат[/b]: TS
+[b]Видео кодек[/b]: H.264
+[b]Аудио кодек[/b]: MP2
+[b]Видео[/b]: MPEG4 Video (H264) 720x576 (16:9) 25fps 2511kbps
+[b]Аудио 1[/b]: MPEG Audio 48000Hz stereo 128kbps [i](Русский)[/i]
+[b]Аудио 2[/b]: MPEG Audio 48000Hz stereo 96kbps [i](Английский)[/i]
+
+[spoiler="Скриншоты"][align=center]
+[URL=http://fastpic.ru/view/83/2016/0904/2b04de8b5d57619a3b42419d9b9e63d9.png.html][IMG]http://i83.fastpic.ru/thumb/2016/0904/d9/2b04de8b5d57619a3b42419d9b9e63d9.jpeg[/IMG][/URL] [URL=http://fastpic.ru/view/82/2016/0904/7a9113ce7e06f703c54e9a866ca76865.png.html][IMG]http://i82.fastpic.ru/thumb/2016/0904/65/7a9113ce7e06f703c54e9a866ca76865.jpeg[/IMG][/URL] [URL=http://fastpic.ru/view/78/2016/0904/ecfc3d6beb9c00c316eedc6af80d598e.png.html][IMG]http://i78.fastpic.ru/thumb/2016/0904/8e/ecfc3d6beb9c00c316eedc6af80d598e.jpeg[/IMG][/URL] [URL=http://fastpic.ru/view/85/2016/0904/92299c90c3cf91835c7b550f0cd1e4e0.png.html][IMG]http://i85.fastpic.ru/thumb/2016/0904/e0/92299c90c3cf91835c7b550f0cd1e4e0.jpeg[/IMG][/URL] [URL=http://fastpic.ru/view/78/2016/0904/aa1c98df30c544958b1432d574d2a4cb.png.html][IMG]http://i78.fastpic.ru/thumb/2016/0904/cb/aa1c98df30c544958b1432d574d2a4cb.jpeg[/IMG][/URL]
+[/align][/spoiler]
+
+[spoiler="Media Info"][pre]
+General
+ID                                       : 1 (0x1)
+Complete name                            : W:\раздачи\\US Open 16. MS R3 Daniel EVANS GBR - San WAWRINKA SUI. Rutracker.org.ts
+Format                                   : MPEG-TS
+File size                                : 4.86 GiB
+Duration                                 : 4h 1mn
+Overall bit rate mode                    : Variable
+Overall bit rate                         : 2 880 Kbps
+
+Video
+ID                                       : 206 (0xCE)
+Menu ID                                  : 706 (0x2C2)
+Format                                   : AVC
+Format/Info                              : Advanced Video Codec
+Format profile                           : High@L3
+Format settings, CABAC                   : Yes
+Format settings, ReFrames                : 4 frames
+Format settings, GOP                     : M=4, N=28
+Codec ID                                 : 27
+Duration                                 : 4h 1mn
+Bit rate                                 : 2 511 Kbps
+Width                                    : 720 pixels
+Height                                   : 576 pixels
+Display aspect ratio                     : 16:9
+Frame rate                               : 25.000 fps
+Standard                                 : PAL
+Color space                              : YUV
+Chroma subsampling                       : 4:2:0
+Bit depth                                : 8 bits
+Scan type                                : Interlaced
+Scan type, store method                  : Separated fields
+Scan order                               : Top Field First
+Bits/(Pixel*Frame)                       : 0.242
+Stream size                              : 4.24 GiB (87%)
+Color range                              : Limited
+Color primaries                          : BT.601 PAL
+Transfer characteristics                 : BT.470 System B, BT.470 System G
+Matrix coefficients                      : BT.601
+
+Audio #1
+ID                                       : 306 (0x132)
+Menu ID                                  : 706 (0x2C2)
+Format                                   : MPEG Audio
+
+
+
+
+[/pre][/spoiler]
+
+[spoiler="Продолжительность"][align=center]
+04:01:32
+[/align][/spoiler]
+
+[spoiler="Счёт"][align=center]
+Daniel EVANS GBR - [b]Stan WAWRINKA SUI [3][/b]
+[b]6[/b]-4, 3-[b]6[/b], [b]7[/b]-6, 6-[b]7[/b], 2-[b]6[/b]
+[/align][/spoiler]"""
+
 
 def smiles(bbc=''):
     for key in sm.keys():
@@ -81,7 +185,8 @@ def smiles(bbc=''):
 
 def zams(bbc=''):
     for key in zam.keys():
-        bbc=bbc.replace(key,zam[key])
+        if bbc.find(key):
+            bbc=bbc.replace(key,zam[key])
     return bbc
 
 def zamena(tag,header,ender,intext='',n=0):
@@ -93,7 +198,6 @@ def zamena(tag,header,ender,intext='',n=0):
         poze=i[0].find(']')
         if pozs != -1:
             par=i[0][pozs+1:poze]
-            par=par.replace('"','')
         else:
             par=''
 
@@ -104,40 +208,19 @@ def zamena(tag,header,ender,intext='',n=0):
         val=test_tag(body,tag)
         if tag == 'IMG':
             intext=intext.replace(original,(header % (par,val[0]))+ender+val[1])
-        elif tag == 'PRE':
+        elif tag in ['PRE']:
             val1=val[0].replace('<br />\n','\n')
             intext=intext.replace(original,(header % (par,))+val1+ender+val[1])
-        elif tag == 'BOX':
-            par1=par.split(',')[0]
-            try:
-                par2=par.split(',')[1]
-            except:
-                par2=''
-            intext=intext.replace(original,(header % (par1,par2))+i[1]+ender)
         else:
             intext=intext.replace(original,(header % (par,))+val[0]+ender+val[1])
     return intext   
 
-def zamena_1(tag,header,intext=''):
-    patt=r'\[%s.*?\]' % (tag)
-    fa=[]
-    fa=re.findall(patt,intext,flags=26)
-    for i in fa:
-        pozs=i.find('=')
-        poze=i.find(']')
-        if pozs != -1:
-            par=i[pozs+1:poze]
-            par=par.replace('"','')
-        else:
-            par=''
-        intext=intext.replace(i,(header % (par,)))
-        intext=intext.replace('[/%s]' % tag.lower(),'</span>')
-    return intext
-
 def zam_sp(intext,n):
     
     patt=r'(\[SPOILER.*?\])(.*?)(\[/SPOILER\])'
-    header='''<div class="sp-wrap"><div class="sp-head folded"><a href="#" onclick="showBlock('%s'); return false"><span class="sp-w">%s</span></a></div><div class="sp-body" id="%s" style="display: none;">'''
+    header='''<div class="sp-wrap"><div class="sp-head folded">
+    <a href="#" onclick="showBlock('%s'); return false"><span>%s</span>
+    </a></div><div class="sp-body"; id="%s"; style="display:none;">'''
     ender='</div></div>'
     
     obj=re.search(patt,intext,flags=26)
@@ -159,12 +242,12 @@ def zam_sp(intext,n):
                 par='Скрытый текст:'
             cat="cat"+str(n)
             val=i[1]
-            test_tag(val,'SPOILER')
+            #test_tag(val,'SPOILER')
             intext=intext.replace(fraza,(header % (cat,par,cat))+val+ender)
         return intext
 
 def zam_html(intext):
-    patt=r'[^]="](https?://[a-z0-9.\-](?:[^\s()<>]+|\([^\s()<>]+\))+(?:\([^\s()<>]+\)|[^\s`!()\[\]{};:\'".,<>?]))'
+    patt=r'[^]=">](https?://[a-z0-9.\-](?:[^\s()<>]+|\([^\s()<>]+\))+(?:\([^\s()<>]+\)|[^\s`!()\[\]{};:\'".,<>?]))'
     repl='<a href="%s">%s</a>'
     
     urls=re.findall(patt,intext)
@@ -172,6 +255,12 @@ def zam_html(intext):
         intext=intext.replace(url,repl % (url,url))
     return intext
     
+def write_html(intext=''):
+    f=open('info.htm','w',encoding='utf-8')
+    f.write(intext)
+    f.close
+    webbrowser.open_new_tab('info.htm')
+
 def test_tag(text,uptag):
     for tag in tags:
         count_start=text.count(tag[0])
@@ -187,54 +276,41 @@ def test_tag(text,uptag):
     return ret
 
 def bbcode2html(intext):
-    Dictag={'IMG':('<img align="%s" alt="" src="%s','">'),
+    Dictag={'IMG':('<img align="%s" src="%s','">'),
             'SIZE':('<span style="font-size: %spx; line-height: normal;">','</span>'),
             'ALIGN':('<span class="post-align" style="text-align: %s;">','</span>'),
             'URL':('<a href="%s">', '</a>'),
-            'FONT':('<span style="font-family: %s;">', '</span>'),
+            'FONT':('<font="%s">', '</font>'),
             'COLOR':('<span class="p-color" style="color: %s;">', '</span>'),
             'PRE':('<pre class="post-pre"%s>', '</pre>'),
             'CODE':('''<div class="c-wrap"><div class="c-head"><b>Код %s:</b></div><div class="c-body">''','</div></div>'),
             'QUOTE':('<div class="q-wrap"><div class="q-head"><span><b>%s</b></span></div><div class="q">','</div></div>'),
-            'QPOST':('', ''),
-            'BOX':('<div class="post-box" style="border-color: %s; background: %s;">','</div>')}
+            'QPOST':('', '')}
 
     intext=intext.replace('\n','<br />\n')
     intext=zams(intext)
-    
     for key in Dictag:
+        #print(key,Dictag.get(key)[0],Dictag.get(key)[1])
         intext=zamena(key,Dictag.get(key)[0],Dictag.get(key)[1],intext)
-    for key in Dictag:
-        if key in ['SIZE','FONT','COLOR']:
-            intext=zamena_1(key,Dictag.get(key)[0],intext)
     n=0
-    intext+='[/spoiler]'
-    while re.search(r'\[SPOILER',intext,flags=26):
+    while re.search(r'\[spoiler',intext,26):
         intext=zam_sp(intext,n)
         n+=1
-    intext=re.sub(r'\[/spoiler\]','',intext,flags=26)    
 
     intext=smiles(intext)
-    intext=intext.replace('''href="tracker.php''','''href="http://rutracker.org/forum/tracker.php''')
-    intext=intext.replace('''href="viewtopic.php?t''','''href="http://rutracker.org/forum/viewtopic.php?t''')
-    intext=intext.replace('[img]','')
     intext=zam_html(intext)
-    intext=intext.replace('''href="http://rutracker.org/forum/viewtopic.php?t''','''href="info.py?tid''')
-    intext=intext.replace('[hr]','<hr>')
-    intext=intext.replace('[/align]','</span>')
-    intext=intext.replace('[/quote]','</div>')
-    intext=intext.replace('[/color]','</span>')
-    intext=intext.replace('[align=center]','<span class="post-align" style="text-align: center">')
-    intext=intext.replace('[/size]','</span>')
-    
-    #intext=intext.replace('</SIZE>','</span>')
-    intext=intext.replace('[/font]','</span>')
-    #intext=intext.replace('</COLOR>','</span>')
-    
-    
-    return intext
+
+    write_html(html_header+intext+html_end)
 
 
 if __name__ == '__main__':
     bbcode2html(content)
 
+"""
+Надо сделать:
+
+    ошибка из-за обратных слешей. их надо экранировать
+    в sel_content вставить запрос на заголовки и хэш для передачи в bbcode2html
+ <[^>]+> - html tag
+
+"""
